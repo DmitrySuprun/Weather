5e6d354533bdc9cb8f6ef4e225d9573a9628948f
Dmitry <suprun1212@gmail.com>
2022-11-01 19:19:50 +0300

    Initial Commit

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,13 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// ViewController
+final class ViewController: UIViewController {
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
